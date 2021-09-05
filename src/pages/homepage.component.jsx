0fb0a192a2a90a4d48a31c6dd0e7b4c2eaefa7f0
NathanMinaev <natanm@gmail.com>
2021-09-06 01:01:30 +0300

    added menu item and menu directory components and styled them

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import DirectoryMenu from "../components/directory-menu/directory-menu.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirectoryMenu />
+  </div>
+);
+
+export default HomePage;
