795abaced5b430812737a6e22df78ba82dfdc2cc
NathanMinaev <natanm@gmail.com>
2021-09-17 18:39:33 +0300

    navigation with router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
 import HomePage from "./pages/homepage.component";
+import { Link, Route, Switch } from "react-router-dom";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+      <Link to={`${props.match.url}13`}>link to hat 13</Link>
+    </div>
+  );
+};
+
+const HatsDetails = (props) => {
+  return (
+    <>
+      <h1>Hats Details</h1>
+      <p>url: {props.match.url}</p>
+      <p>Id: {props.match.params.id}</p>
+    </>
+  );
+};
+
+const JacketsPage = (props) => {
+  return <h1>Jackets Page</h1>;
+};
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route path="/shop/hats/:id" component={HatsDetails} />
+      <Route exact path="/shop/jackets" component={JacketsPage} />
+    </Switch>
   );
 }
 
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,10 +1,16 @@
 import DirectoryMenu from "../components/directory-menu/directory-menu.component";
 import "./homepage.styles.scss";
+import { Link, withRouter } from "react-router-dom";
 
-const HomePage = () => (
+const HomePage = (props) => (
   <div className="homepage">
+    <Link to="shop/hats">Hats</Link>
+    <Link to="shop/jackets">Jackets</Link>
+    <button onClick={() => props.history.push("shop/hats")}>
+      Hats via history
+    </button>
     <DirectoryMenu />
   </div>
 );
 
-export default HomePage;
+export default withRouter(HomePage);
